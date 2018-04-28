ce6d06e3b76f4e0c97b349d77c4322f13941edc5
Matt <mgravish@gmail.com>
2018-04-28 15:49:12 -0700

    updatesssss

--- lottie-animations.js
+++ lottie-animations.js
@@ -1,38 +1,84 @@
-console.log('Loaded lottie-animations.js');
+var files = [ 
+    'lottie/pill-button.json',
+    'lottie/radio-button.json'
+];
 
-var anim1 = 'lottie/button--disabled.json';
+var animArray = new Array();
+var container = $('#centered');
 
-var params = {
+for (var i=0; i<files.length; i++) {
+    
+    var div =`
+    <div class="lottie-container" id="lottie-anim-`+i+`">
+        <div class="reload"></div>
+        <div class="toggle"></div>
+    </div>`;
+    container.append(div);
+    var elem = $('.lottie-container')[i];
+    
+    params = {
+        path: files[i],
+        container: elem,
         renderer: 'svg',
         loop: false,
-        autoplay: false,
-        path: null,
-};
-
-var anim;
-var animArray = new Array();
-var element = '<div id="anim1"></div>';
-var elem;
-
-for(var i = 0; i < 1; i++) {
-    //elem = $('#centered')append(element);
-    params.path = anim1;
-    params.name = 'mybutton';
-    //params.container: elem;
-    params.container.on('click', clicked);
-    anim = lottie.loadAnimation(params);
-    animArray.push(anim);
+        autoplay: false
+    }
+    
+    var anim = lottie.loadAnimation(params);
+    anim.goToAndStop(0,true);
+    waitForDOMLoaded(elem.id, anim);
+    animArray[i] = anim;    
 }
 
 
-
-function startAnimation(){
-    console.log ('loaded!');
-    var container = document.getElementById('button--disabled');
-    container.onclick = clicked;
+function waitForDOMLoaded(e, anim) {
+    anim.addEventListener('DOMLoaded', function() {
+        prepAnimElement(e, anim);
+    });
+}
+                          
+function prepAnimElement(e, anim) {
+    addListeners(e, anim);
 }
 
-function clicked(e){
-    console.log(this);
-    mybutton.goToAndPlay(0);
+function addListeners (e, anim) {
+    var fn = anim.fileName;
+    anim.toggled = false;
+    anim.segs = [0,30];
+    var elem = $('#'+anim.fileName)[0];
+    var parent = $(elem).parent()[0];
+    var svgParent = $(parent).parent()[0];
+    var container = $(svgParent).parent()[0];
+    var rl = '#'+e;
+    var rlbtn = $(rl + ' .reload')[0];
+    var togglebtn = $(rl + ' .toggle')[0];
+    
+    parent.addEventListener('click', function() { 
+        if(!anim.toggled){ anim.segs = [0,30]; }
+        else { anim.segs = [35,60]; }
+        anim.playSegments(anim.segs, true);
+    }, false);
+    
+    rlbtn.addEventListener('click', function() { anim.goToAndStop(0, true); }, false);
+    
+    togglebtn.addEventListener('click', function() {
+        if( !anim.toggled ) { 
+            $(togglebtn).addClass('toggle-enabled');
+        }
+        else { 
+            $(togglebtn).removeClass('toggle-enabled'); 
+        }
+        anim.toggled = !anim.toggled;
+        console.log('Toggling status: '+anim.toggled);
+    }, false);
+    
+    $(parent).css('cursor','pointer');
+    console.log(container);
+    $(container).mouseover(function(){
+        console.log('hovering');
+    });
+}
+
+function reload() {
+    console.log ('reload');
 }
